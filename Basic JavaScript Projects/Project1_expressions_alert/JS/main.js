25ea5561140611dd300ca7343d218057ca62230b
Josh Hill <joshuaamhill@gmail.com>
2023-03-08 14:23:25 -0800

    First commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+let days = 42;
+let x = `It's been ` + days + ` since the accident`;
+window.alert(x);
+alert(sum);
+
+// THis is a comment
+
+let sent1 = `This is the beginning of the string`;
+let sent2 = `and this is the end of the string`;
+const sentConcat = sent1 + sent2; // This concats stuff
+let sum = 3 + 3; // This adds stuff
