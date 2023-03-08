308fec803513379230595e60948fc4fa144b73f2
Josh Hill <joshuaamhill@gmail.com>
2023-03-08 14:34:52 -0800

    Added onClick function

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,7 @@
 let days = 42;
 let x = `It's been ` + days + ` since the accident`;
-window.alert(x);
-alert(sum);
+// window.alert(x);
+// alert(sum);
 
 // THis is a comment
 
@@ -9,3 +9,8 @@ let sent1 = `This is the beginning of the string`;
 let sent2 = `and this is the end of the string`;
 const sentConcat = sent1 + sent2; // This concats stuff
 let sum = 3 + 3; // This adds stuff
+
+function onClick() {
+  let button = document.getElementById("button");
+  button.innerHTML = "Sup";
+}
