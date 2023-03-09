038379f8f8642ad25528383eeaa1f23334f32e7c
Josh Hill <joshuaamhill@gmail.com>
2023-03-08 18:52:04 -0800

    First commit

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,9 @@
+let x = 10;
+
+document.write(typeof x);
+document.write(x + "10");
+document.write(0 / 0);
+document.write(isNaN("hey"));
+document.write(2e3120);
+console.log("10" == 10);
+console.log("10" === 10);
